bab26bc42fc37985d71c893808bd7fc33f65b3c8
Santoshpa <santoshpanta50@gmail.com>
2025-03-31 04:14:09 +1100

    removed code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function DashboardPage(){
     return (
     <div className="min-h-screen rounded bg-gray-100 p-6">
       <h1 className="text-3xl text-black font-bold mb-4">Admin Dashboard</h1>
-      <p className="text-black">Welcome to the Admin's Dashboard!</p>
+      <p className="text-black">Welcome to the Admins Dashboard!</p>
     </div>
     );
 }
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,7 +2,7 @@ export default function DashboardPage(){
     return (
     <div className="min-h-screen rounded bg-gray-100 p-6">
       <h1 className="text-3xl text-black font-bold mb-4">User Dashboard</h1>
-      <p className="text-black">Welcome to the User's Dashboard!</p>
+      <p className="text-black">Welcome to the Users Dashboard!</p>
     </div>
     );
 }
